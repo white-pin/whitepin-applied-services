d1b3d52b2d5b12a688ee760db7943de9537e668d
김상열 <sangkimyul@ibizsoftware.net>
2019-09-19 14:28:41 +0900

    no message

--- airbnb/src/main/java/com/github/airbnb/config/swagger/SwaggerConfig.java
+++ airbnb/src/main/java/com/github/airbnb/config/swagger/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
     public Docket petApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage( "com.github.airbnb.controller" ))
+                .apis(RequestHandlerSelectors.basePackage( "com.github.airbnb.controller.api" ))
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/")
