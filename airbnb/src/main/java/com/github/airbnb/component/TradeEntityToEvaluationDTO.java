4f0a0b7d788f272462b3ab79d2f4c3ef312ca5cd
limyongtae <lyt032410@gmail.com>
2019-09-24 14:43:28 +0900

    no message

--- airbnb/src/main/java/com/github/airbnb/component/TradeEntityToEvaluationDTO.java
+++ airbnb/src/main/java/com/github/airbnb/component/TradeEntityToEvaluationDTO.java
@@ -2,6 +2,7 @@ package com.github.airbnb.component;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.github.airbnb.dto.ProductDTO.EvaluationDTO;
 import com.github.airbnb.entity.TradeEntity;
@@ -17,9 +18,22 @@ public class TradeEntityToEvaluationDTO implements Converter<TradeEntity, Evalua
                     .score3(trade.getEvaluationScore3())
                     .score4(trade.getEvaluationScore4())
                     .score5(trade.getEvaluationScore5())
+                    .scoreTotal(getTotal(trade))
                     .review(trade.getEvaluationReview())
                     .date(trade.getEvaluationDate())
                     .build();
     }
-
+    
+    private String getTotal(TradeEntity trade) {
+    	int totalScore = chkEmpty(trade.getEvaluationScore1());
+    	totalScore += chkEmpty(trade.getEvaluationScore2());
+    	totalScore += chkEmpty(trade.getEvaluationScore3());
+    	totalScore += chkEmpty(trade.getEvaluationScore4());
+    	totalScore += chkEmpty(trade.getEvaluationScore5());
+    	return String.valueOf(totalScore);
+    }
+    
+    private int chkEmpty(String s) {
+    	return StringUtils.isEmpty(s) ? 0 : Integer.parseInt(s);
+    }
 }
